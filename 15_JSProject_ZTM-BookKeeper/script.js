59bc472529889ef1a7eadeb7abc0b22f29e01f55
Arnor Ameti <arnameti@gmail.com>
2022-05-20 18:03:02 +0200

    Implementing the open/close functionality of the modal

--- 15_JSProject_ZTM-BookKeeper/script.js
+++ 15_JSProject_ZTM-BookKeeper/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameEL = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmarks-container');
+
+// Show Modal, Focus on Input
+
+const showModal = function () {
+  modal.classList.add('show-modal');
+  websiteNameEL.focus();
+};
+
+//  Modal Event Listener
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () =>
+  modal.classList.remove('show-modal')
+);
+window.addEventListener('click', e =>
+  e.target === modal ? modal.classList.remove('show-modal') : false
+);
