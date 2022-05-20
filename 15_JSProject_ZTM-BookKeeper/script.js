b05257c7da7df4813af8f8983e821575b3c0c150
Arnor Ameti <arnameti@gmail.com>
2022-05-20 21:15:10 +0200

    Form Validation

--- 15_JSProject_ZTM-BookKeeper/script.js
+++ 15_JSProject_ZTM-BookKeeper/script.js
@@ -1,5 +1,17 @@
 'use strict';
 
+/*
+// Old Version
+if (!urlValue.includes('http://', 'https://')) {     
+    urlValue = `https://${urlValue}`; 
+} 
+ 
+// New Version
+if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
+     urlValue = `https://${urlValue}`; 
+}
+*/
+
 const modal = document.getElementById('modal');
 const modalShow = document.getElementById('show-modal');
 const modalClose = document.getElementById('close-modal');
@@ -8,8 +20,9 @@ const websiteNameEL = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
-// Show Modal, Focus on Input
+let bookmarks = [];
 
+// Show Modal, Focus on Input
 const showModal = function () {
   modal.classList.add('show-modal');
   websiteNameEL.focus();
@@ -23,3 +36,70 @@ modalClose.addEventListener('click', () =>
 window.addEventListener('click', e =>
   e.target === modal ? modal.classList.remove('show-modal') : false
 );
+
+const showMessage = function (message) {
+  alert(message);
+};
+
+const nameAndUrlAreValid = function (nameValue, urlValue) {
+  return !(!nameValue || !urlValue);
+};
+
+const urlIsValid = function (urlValue) {
+  const expression =
+    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+  const regex = new RegExp(expression);
+
+  return urlValue.match(regex);
+};
+
+// Validate Form
+const validate = function (nameValue, urlValue) {
+  // const expression =
+  //   /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+  // const regex = new RegExp(expression);
+
+  // if (!nameValue || !urlValue) {
+  //   alert('Please submit values for both fields');
+  //   return false;
+  // }
+
+  // if (!urlValue.match(regex)) {
+  //   alert('Please provide a valid web address');
+  //   return false;
+  // }
+
+  // Valid
+  if (!nameAndUrlAreValid(nameValue, urlValue)) {
+    showMessage('Please submit values for both fields');
+  }
+
+  if (!urlIsValid(urlValue)) showMessage('Please provide a valid web address');
+
+  return nameAndUrlAreValid(nameValue, urlValue) && urlIsValid(urlValue);
+};
+
+// Handle Data from Form
+const storeBookmark = function (e) {
+  e.preventDefault();
+  const nameValue = websiteNameEL.value;
+  let urlValue = websiteUrlEl.value;
+
+  if (!urlValue.includes('https://') && !urlValue.includes('http://'))
+    urlValue = `https://${urlValue}`;
+
+  if (!validate(nameValue, urlValue)) return;
+
+  const bookmark = {
+    name: nameValue,
+    url: urlValue,
+  };
+
+  bookmarks.push(bookmark);
+  console.log(bookmarks);
+  bookmarkForm.reset();
+  websiteNameEL.focus();
+};
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark);
